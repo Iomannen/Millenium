75e950eb217f96b405e575a0eff4623f13283102
Iomannen <maxymdavidov@gmail.com>
2024-10-22 13:04:28 +0300

    Update swiper.js

--- swiper.js
+++ swiper.js
@@ -4,6 +4,7 @@ const swiper = new Swiper('.swiper', {
     pagination: {
       el: '.swiper-pagination',
       type: 'bullets',
+      clickable: true,
     },
       // Navigation arrows
   navigation: {
