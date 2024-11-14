3f58703f9fe310cccba671a2a2945bb58508a230
Iomannen <maxymdavidov@gmail.com>
2024-11-14 22:59:30 +0300

    Delete arrowbutton.js

--- arrowbutton.js
+++
@@ -1,8 +0,0 @@
-let arrowButton = document.querySelector('.cards__button');
-let cardsSection = document.querySelector('.cards-list');
-arrowButton.addEventListener('click', function() {
-    cardsSection.classList.toggle('cards-hidden');
-    cardsSection.classList.toggle('cards-list');
-    arrowButton.classList.toggle('cards__button-active');
-    arrowButton.classList.toggle('cards__button');
-})
