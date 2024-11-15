949bf92195806d7472bf5f99e621701ee4386699
Iomannen <maxymdavidov@gmail.com>
2024-11-15 14:43:20 +0300

    Update sertificates-swiper.js

--- js/sertificates-swiper.js
+++ js/sertificates-swiper.js
@@ -1,7 +1,7 @@
-let qwl = window.matchMedia('(max-width: 1279px)');
+let windowCheck = window.matchMedia('(max-width: 1279px)');
 let sertificateSwiper = document.querySelector('.swiper2');
 window.addEventListener('DOMContentLoaded', () => {
-  if (qwl.matches) {
+  if (windowCheck.matches) {
 sertificateSwiper.classList.toggle('swiper');
 let swiper = new Swiper('.swiper', {
     loop: true,
